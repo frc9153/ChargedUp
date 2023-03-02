4953e0af21d3210c4ac74dc35b80f438a77ee0de
one-some <onesome+noreply@github.com>
2023-03-02 16:43:36 -0600

    Claw: Move to Talon

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -4,59 +4,51 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMaxAnalogSensor;
-import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Claw extends SubsystemBase {
-  private CANSparkMax m_clawMotor;
-  private SparkMaxPIDController m_clawPIDController;
-  private SparkMaxAnalogSensor m_clawMotorPot;
+  private TalonSRX m_clawMotor;
   
   // Glorious hack because we can't read setpoint from motor controller
   private double m_setPoint;
 
   public Claw() {
-    m_clawMotor = new CANSparkMax(Constants.Claw.clawMotorID, MotorType.kBrushless);
-    
-    m_clawPIDController = m_clawMotor.getPIDController();
-    m_clawMotorPot = m_clawMotor.getAnalog(SparkMaxAnalogSensor.Mode.kAbsolute);
+    m_clawMotor = new TalonSRX(Constants.Claw.clawMotorID);
 
-    // PID Config setup
-    m_clawPIDController.setP(Constants.Claw.clawP);
-    m_clawPIDController.setI(Constants.Claw.clawI);
-    m_clawPIDController.setD(Constants.Claw.clawD);
-    m_clawPIDController.setIZone(Constants.Claw.clawIZone);
-    m_clawPIDController.setFF(Constants.Claw.clawFF);
-    m_clawPIDController.setOutputRange(Constants.Claw.minClawSpeed, Constants.Claw.maxClawSpeed);
-    m_clawPIDController.setFeedbackDevice(m_clawMotorPot);
-
-    m_clawMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    // Config
+    m_clawMotor.config_kP(0, Constants.Claw.clawP);
+    m_clawMotor.config_kI(0, Constants.Claw.clawI);
+    m_clawMotor.config_kD(0, Constants.Claw.clawD);
+    m_clawMotor.config_IntegralZone(0, Constants.Claw.clawIZone);
     
-    // Make config survive reboot (learn from others' mistakes!)
-    m_clawMotor.burnFlash();
+    // FIXME: This might be the wrong value
+    m_clawMotor.config_kF(0, Constants.Claw.clawFF);
+
+    // FIXME: This is also probably not the right value
+    m_clawMotor.configAllowableClosedloopError(0, Constants.Claw.clawPIDEpsilon);
+
+    // FIXME: 100 as guess value. We'll find out what that does
+    m_clawMotor.configPeakOutputForward(Constants.Claw.maxClawSpeed, 100);
+    m_clawMotor.configPeakOutputReverse(Constants.Claw.minClawSpeed, 100);
+
+    m_clawMotor.configSelectedFeedbackSensor(FeedbackDevice.Analog);
   }
 
   public void setSetPoint(double setPoint) {
     m_setPoint = setPoint;
-    m_clawPIDController.setReference(setPoint, ControlType.kPosition);
-  }
-
-  public void setSpeed(double speed) {
-    m_clawMotor.set(speed);
+    m_clawMotor.set(ControlMode.Position, m_setPoint);
   }
 
   public boolean isAtSetPoint() {
-    return Math.abs(m_setPoint - m_clawMotorPot.getPosition()) <= Constants.Claw.clawPIDEpsilon;
+    return Math.abs(m_setPoint - m_clawMotor.getSelectedSensorPosition()) <= Constants.Claw.clawPIDEpsilon;
   }
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  public void periodic() {}
 }
